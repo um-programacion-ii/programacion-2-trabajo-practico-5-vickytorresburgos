8aad7655f04ed6e3e17f920017b3edec37bcfea0
vicky <vicky@pop-os.localdomain>
2025-07-16 22:20:24 -0300

    configuracion de las relaciones entre las entidades jpa

--- tp5/src/main/java/com/example/tp5/models/Proyecto.java
+++ tp5/src/main/java/com/example/tp5/models/Proyecto.java
@@ -1,20 +1,38 @@
 package com.example.tp5.models;
 
+
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
+import java.time.LocalDate;
+import java.util.*;
+
+@Entity
+@Table(name = "proyectos")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Proyecto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String nombre;
+
+    @Column(length = 1000)
     private String descripcion;
+
+    @Column(name = "fecha_inicio")
+    @Temporal(TemporalType.DATE)
     private LocalDate fechaInicio;
+
+    @Column(name = "fecha_fin")
+    @Temporal(TemporalType.DATE)
     private LocalDate fechaFin;
+
+    @ManyToMany(mappedBy = "proyectos")
     private Set<Empleado> empleados = new HashSet<>();
 }
