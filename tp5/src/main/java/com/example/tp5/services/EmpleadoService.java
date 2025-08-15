a7a64daef5af93ddab68085ecf31206f0d3fad7a
vicky <vicky@pop-os.localdomain>
2025-08-15 14:14:58 -0300

    adding empleado service

---
+++ tp5/src/main/java/com/example/tp5/services/EmpleadoService.java
@@ -0,0 +1,17 @@
+package com.example.tp5.services;
+
+import com.example.tp5.models.Empleado;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface EmpleadoService {
+    Empleado guardar(Empleado empleado);
+    Empleado buscarPorId(Long id);
+    List<Empleado> buscarPorDepartamento(String nombreDepartamento);
+    List<Empleado> buscarPorRangoSalario(BigDecimal salarioMin, BigDecimal salarioMax);
+    BigDecimal obtenerSalarioPromedioPorDepartamento(Long departamentoId);
+    List<Empleado> obtenerTodos();
+    Empleado actualizar(Long id, Empleado empleado);
+    void eliminar(Long id);
+}
